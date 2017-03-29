7915a3aad2c2ebfdbc9b76085e66da36e4ac3607
otake <otake.hiroyo@gmail.com>
2017-03-29 13:58:45 +0900

    投稿画面作成

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>感想投稿フォーム</title>
+	</head>
+	<body>
+		<h1>感想投稿フォーム</h1>
+		<form action="inquiry.php" method="post">
+			名前:<input type="text" name="name">
+			感想:<input type="text" name="impression">
+			<input type="submit" value="感想を投稿"
+		</form>
+	</body>
+</html>
